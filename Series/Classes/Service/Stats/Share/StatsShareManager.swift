6d64bfb3be9a1b08e1b9b2f52f295dcc3e20211e
Sergei Pankov <sergeipankov@Sergeis-MacBook-Air.local>
2022-05-11 13:03:00 +0100

    config

--- Series/Classes/Service/Stats/Share/StatsShareManager.swift
+++ Series/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.series.widget")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.series7.widget")
     }
     
     @discardableResult
